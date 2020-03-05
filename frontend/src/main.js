eb00a69fee219e7c7c55c2c4dff3811454ac4205
andrzejtrybull <andrzej.trybull.dev@gmail.com>
2020-03-05 23:21:23 +0100

    first approach to login user

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import HelloWorld from "./components/HelloWorld";
+import LoginForm from "./components/LoginForm";
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: HelloWorld
+  },
+  {
+    path: '/loginform',
+    component: LoginForm
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
